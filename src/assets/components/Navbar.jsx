7f75d95268a09ddd72e202e25c02ed857a8d64b4
Ramiro Renteria <Ramis28@yahoo.com>
2024-04-10 21:51:52 -0700

    adding user functionality on home page

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-
+import { useState, useEffect } from 'react'
+import { getTasks, createTask, deleteTask, updateTask } from '../../ApiServices/TasksService'
 import { removeJwt } from '../../ApiServices/JwtService'
+import { getUser } from '../../ApiServices/UserService'
 
 const Navbar = () => {
   const navigate = useNavigate()
@@ -11,10 +13,21 @@ const Navbar = () => {
     navigate('/')
   }
 
+const [currentUser, setCurrentUser] = useState(null)
+
+useEffect(() => {
+  fetchUser()
+}, [])
+
+const fetchUser = async () => {
+  const user = await getUser()
+  setCurrentUser(user)
+}
+// console.log(fetchUser)
   return (
     <div className='nav--content'>
       <img className='img--nav--edit' src='./src/images/Tusker-Remake-100-cutout 1.png'/>
-      <h2 className='text--nav--edit'>Welcome, Ramiro</h2>
+      {currentUser && <h2 className='text--nav--edit'>Welcome, {currentUser.user_name}</h2>}
       <div className='button--nav--container'>
         {/* use ApiServices to make this functional, also add a function above to deal with this event */}
         <button className='button--nav--edit' onClick={handleLogout}>Log Out</button>
